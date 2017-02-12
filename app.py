469ff1f90cec0ae7a5ee442f7e0a75a36a2b05b4
Kyu Lee <smailq@gmail.com>
2017-02-11 17:31:56 -0800

    python

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
+
+if __name__ == "__main__":
+    app.run()
