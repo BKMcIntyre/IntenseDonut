42c1db115dedf6e9b3954c1bd3114be97888e5d7
BKMcIntyre <baileykmcintyre@gmail.com>
2017-02-12 02:24:37 -0800

    D3 chart draw

--- static/data.js
+++ static/data.js
@@ -4,10 +4,19 @@ $(document).ready(function() {
         if (f) {
             var r = new FileReader();
             r.onload = function(e) {
+                window.filter_data = [];
                 window.har_data = $.parseJSON(e.target.result);
                 $.each(har_data.log.entries, function( index, value ) {
-                    delete value.response.content.text;
+                    window.filter_data.push({
+                        index: index,
+                        url: value.request.url,
+                        method: value.request.method,
+                        ipaddress: value.serverIPAddress,
+                        status: value.response.status,
+                        type: value.response.content.mimeType
+                    });
                 });
+                window.dispatchEvent(new CustomEvent('dataChanged'));
             }
             r.readAsText(f);
         } else {
