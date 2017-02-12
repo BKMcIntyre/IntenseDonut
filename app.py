58f2b6086831f8a79953d1599c67c8f9bd980a16
Kyu Lee <smailq@gmail.com>
2017-02-11 17:49:05 -0800

    Add basic route

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, static_url_path="")
 
 @app.route("/")
 def hello():
     return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
 
 
 if __name__ == "__main__":
