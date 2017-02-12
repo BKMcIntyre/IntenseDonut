f26ee388fb38b92eb60ddc4bb9f248366ac83696
Mia Marukot <mia@venngage.com>
2017-02-11 19:37:56 -0800

    Some UI and global variable har_data stored

--- static/data.js
+++ static/data.js
@@ -1,1 +1,17 @@
-console.log("hi");
+$(document).ready(function() {
+    $('#upload').change((e)=>{
+        var f = e.target.files[0];
+        if (f) {
+            var r = new FileReader();
+            r.onload = function(e) {
+                window.har_data = $.parseJSON(e.target.result);
+                $.each(har_data.log.entries, function( index, value ) {
+                    delete value.response.content.text;
+                });
+            }
+            r.readAsText(f);
+        } else {
+            alert("Failed to load file");
+        }
+    });
+});
