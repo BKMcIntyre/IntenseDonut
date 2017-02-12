361240a115147d6bc542f32603c2b856e2798474
Kyu Lee <smailq@gmail.com>
2017-02-12 08:19:14 -0800

    More impl

--- static/data.js
+++ static/data.js
@@ -5,7 +5,28 @@ var getLocation = function(href) {
     return l;
 };
 
+window.filter_settings = [];
+
 $(document).ready(function() {
+
+
+
+    $('#render').click(  (e) => {
+
+        $('select').each( function() {
+
+          //  console.log($(this).val());
+            window.filter_settings.push($(this).val());
+        });
+
+        //console.log(window.filter_settings);
+
+        window.dispatchEvent(new CustomEvent('dataChanged'));
+    });
+
+
+
+
     $('#upload').change((e)=>{
         var f = e.target.files[0];
         if (f) {
@@ -15,20 +36,7 @@ $(document).ready(function() {
                 window.har_data = $.parseJSON(e.target.result);
                 $.each(har_data.log.entries, function( index, value ) {
 
-                    if (value.response.httpVersion == 'HTTP/2.0') {
-                        console.log(value);
-                    }
-
-                    var host = 'Unknown';
-
-                    $.each(value.request.headers, function (i) {
-                        var v = value.request.headers[i];
-                        if (v.name == "Host" || v.name == ":authority") {
-                            host = v.value;
-                        }
-                    });
-
-                    var type = 'Unknown';
+                    var type = 'Not set';
 
                     if (value.response.content.mimeType.indexOf(';') > 0) {
                         type = value.response.content.mimeType.substring(0, value.response.content.mimeType.indexOf(';'));
@@ -44,12 +52,13 @@ $(document).ready(function() {
                         status: "" + value.response.status,
                         type: type,
                         time: value.time,
-                        host: host,
+                        host: l.hostname,
                         httpVersion: value.response.httpVersion,
                         path: l.pathname
                     });
                 });
-                window.dispatchEvent(new CustomEvent('dataChanged'));
+
+                $('#selections').show();
             }
             r.readAsText(f);
         } else {
